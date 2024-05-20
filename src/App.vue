4e505e77ce44d5bb3430760e3597455689256c53
babyblue <lijl@blackbeanssoft.com>
2024-05-20 10:37:42 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,20 @@
       width="125"
       height="125"
     />
-    <Editor
-      class="editor"
-      api-key="34ac7u4b918wknmfwu000e5a32lre5ey4v1hek9njuc6xw53"
-      :init="init"
-    />
+    <Editor class="editor" :init="init" />
   </main>
 </template>
 <script lang="ts" setup>
   import Editor from '@tinymce/tinymce-vue'
+  import tinymce from 'tinymce'
+  // import '../public/plugin.min.js'
+  import { onMounted } from 'vue'
 
   const init = {
     selector: '.editor',
     external_plugins: {
       tiny_mce_wiris: `/node_modules/@wiris/mathtype-tinymce6/plugin.min.js`,
+      powerpaste: '/plugin.min.js',
     },
     extended_valid_elements: '*[.*]',
     draggable_modal: true,
@@ -29,13 +29,18 @@
     branding: false, // tiny 技术支持信息是否显示
     menubar: false, // 菜单栏
     statusbar: false, // 注意将 statusbar 设置为 false 来隐藏字数统计显示
-    plugins: 'image media',
-    toolbar: `fontsize | lineheight fontfamily | bold italic forecolor | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry | image media `,
+    plugins: 'image media powerpaste',
+    // powerpaste_allow_local_images: true,
+    // powerpaste_word_import: 'prompt',
+    // powerpaste_html_import: 'prompt',
+    toolbar: `fontsize | lineheight fontfamily | bold italic forecolor | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry | image media`,
     fontfamily: '微软雅黑;',
     line_height_formats: '1 1.2 1.4 1.6 2', // 行高
     font_size_formats:
       '12pt 14pt 16pt 18pt 20pt 22pt 24pt 28pt 32pt 36pt 48pt 56pt 72pt', // 字体大小
     file_picker_types: 'media image',
+    image_prepend_url: 'https://www.example.com/images/',
+
     file_picker_callback: (cb: any, value: any, meta: any) => {
       const input = document.createElement('input')
       input.setAttribute('type', 'file')
@@ -43,5 +48,9 @@
       input.click()
     },
   }
+
+  onMounted(() => {
+    tinymce.init(init)
+  })
 </script>
 <style lang="less" scoped></style>
